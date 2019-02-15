7e9b4e115c8694321af1ed9a60e2875579275f24
iamchristopher <chriswright@live.com>
2019-02-14 20:48:14 -0500

    Changed bundle is now minified

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
+import {
+    uglify,
+} from 'rollup-plugin-uglify';
 
 export default {
     input: 'source/index.js',
@@ -23,6 +26,7 @@ export default {
         commonjs(),
         replace({
             'process.env.NODE_ENV': JSON.stringify('production')
-        })
+        }),
+        uglify(),
     ],
 };
