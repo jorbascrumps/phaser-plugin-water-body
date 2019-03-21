482779eb76454c1df05a17c64fcd97c12a9713a2
jorbascrumps <chriswright@live.com>
2019-03-21 19:03:56 -0400

    Replace uglify plugin with Babel minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import {
-    uglify,
-} from 'rollup-plugin-uglify';
+import minify from 'rollup-plugin-babel-minify';
 
 export default {
     input: 'source/index.js',
@@ -24,9 +21,8 @@ export default {
             browser: true
         }),
         commonjs(),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify('production')
+        minify({
+            comments: false,
         }),
-        uglify(),
     ],
 };
